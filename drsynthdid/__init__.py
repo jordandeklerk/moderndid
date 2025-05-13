d7df26217a1be4977d88a43f69108db7fc2e2431
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-05-12 23:22:13 -0400

    refactor: rename preprocessing module

--- drsynthdid/__init__.py
+++ drsynthdid/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import
 """Doubly Robust DiD with Synthetic Controls."""
 
-from .preprocessing import pre_process_drdid
+from .preprocess import pre_process_drdid
 
 __all__ = ["pre_process_drdid"]
