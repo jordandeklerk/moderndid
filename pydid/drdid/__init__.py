6a4ea25e4858895535207b9dd1eb9c5f0766b6bc
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-05-24 12:41:40 -0400

    feat: add wboot_drdid_imp_panel to exports in the DiD module

--- pydid/drdid/__init__.py
+++ pydid/drdid/__init__.py
@@ -1,7 +1,13 @@
 """Doubly robust DiD estimators."""
 
 from .aipw_estimators import aipw_did_panel, aipw_did_rc_imp1, aipw_did_rc_imp2
-from .boot import wboot_drdid_ipt_rc1, wboot_drdid_ipt_rc2, wboot_drdid_rc_imp1, wboot_drdid_rc_imp2
+from .boot import (
+    wboot_drdid_imp_panel,
+    wboot_drdid_ipt_rc1,
+    wboot_drdid_ipt_rc2,
+    wboot_drdid_rc_imp1,
+    wboot_drdid_rc_imp2,
+)
 from .pscore_ipt import calculate_pscore_ipt
 from .wols import wols_panel, wols_rc
 
@@ -9,6 +15,7 @@ __all__ = [
     "aipw_did_panel",
     "aipw_did_rc_imp1",
     "aipw_did_rc_imp2",
+    "wboot_drdid_imp_panel",
     "wboot_drdid_rc_imp1",
     "wboot_drdid_rc_imp2",
     "wboot_drdid_ipt_rc1",
