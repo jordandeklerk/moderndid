14c096cb6615e3ee6a20bcaf440adf37ccae55db
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-06-08 22:20:35 -0400

    feat: add wboot_reg_rc estimator

--- pydid/drdid/__init__.py
+++ pydid/drdid/__init__.py
@@ -12,6 +12,7 @@ from .bootstrap_panel import (
 )
 from .bootstrap_rc import wboot_drdid_rc1, wboot_drdid_rc2
 from .bootstrap_rc_ipt import wboot_drdid_ipt_rc1, wboot_drdid_ipt_rc2
+from .bootstrap_reg_rc import wboot_reg_rc
 from .ipw_estimators import ipw_did_rc
 from .pscore_ipt import calculate_pscore_ipt
 from .wols import wols_panel, wols_rc
@@ -30,6 +31,7 @@ __all__ = [
     "wboot_ipw_panel",
     "wboot_ipw_rc",
     "wboot_reg_panel",
+    "wboot_reg_rc",
     "wboot_std_ipw_panel",
     "wboot_twfe_panel",
     "wols_panel",
