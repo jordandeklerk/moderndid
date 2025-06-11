d4b1862f7bd09f41768df958ed4e7d63f0211241
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-06-10 21:11:14 -0400

    feat: add panel and RC utility functions

--- pydid/__init__.py
+++ pydid/__init__.py
@@ -20,12 +20,35 @@ from pydid.drdid.boot_twfe_rc import wboot_twfe_rc
 from pydid.drdid.ipw_estimators import ipw_did_rc
 from pydid.drdid.pscore_ipt import calculate_pscore_ipt
 from pydid.drdid.wols import wols_panel, wols_rc
+from pydid.utils import (
+    are_varying,
+    complete_data,
+    convert_panel_time_to_int,
+    create_relative_time_indicators,
+    datetime_to_int,
+    extract_vars_from_formula,
+    fill_panel_gaps,
+    is_panel_balanced,
+    is_repeated_cross_section,
+    long_panel,
+    make_panel_balanced,
+    panel_has_gaps,
+    panel_to_cross_section_diff,
+    parse_formula,
+    prepare_data_for_did,
+    unpanel,
+    validate_treatment_timing,
+    widen_panel,
+)
 
 __all__ = [
+    # Core DiD estimators
     "aipw_did_panel",
     "aipw_did_rc_imp1",
     "aipw_did_rc_imp2",
     "ipw_did_rc",
+    "calculate_pscore_ipt",
+    # Bootstrap functions
     "mboot_did",
     "mboot_twfep_did",
     "wboot_dr_tr_panel",
@@ -42,7 +65,26 @@ __all__ = [
     "wboot_std_ipw_rc",
     "wboot_twfe_panel",
     "wboot_twfe_rc",
+    # Regression functions
     "wols_panel",
     "wols_rc",
-    "calculate_pscore_ipt",
+    # Panel data utilities
+    "are_varying",
+    "complete_data",
+    "convert_panel_time_to_int",
+    "create_relative_time_indicators",
+    "datetime_to_int",
+    "extract_vars_from_formula",
+    "fill_panel_gaps",
+    "is_panel_balanced",
+    "is_repeated_cross_section",
+    "long_panel",
+    "make_panel_balanced",
+    "panel_has_gaps",
+    "panel_to_cross_section_diff",
+    "parse_formula",
+    "prepare_data_for_did",
+    "unpanel",
+    "validate_treatment_timing",
+    "widen_panel",
 ]
