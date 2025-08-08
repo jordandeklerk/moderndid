827ca5264c7ca7afc97ee0cf58ce2369ec72dca7
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2025-08-07 22:28:35 -0400

    feat: add spline utils

---
+++ moderndid/didcont/spline/__init__.py
@@ -0,0 +1,31 @@
+"""Spline Functions for Continuous Treatment DiD."""
+
+from .utils import (
+    append_zero_columns,
+    arrays_almost_equal,
+    compute_quantiles,
+    create_string_sequence,
+    drop_first_column,
+    filter_within_bounds,
+    has_duplicates,
+    is_close,
+    linspace_interior,
+    reverse_cumsum,
+    to_1d,
+    to_2d,
+)
+
+__all__ = [
+    "is_close",
+    "arrays_almost_equal",
+    "has_duplicates",
+    "reverse_cumsum",
+    "filter_within_bounds",
+    "compute_quantiles",
+    "linspace_interior",
+    "to_1d",
+    "to_2d",
+    "drop_first_column",
+    "append_zero_columns",
+    "create_string_sequence",
+]
