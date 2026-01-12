de8657ddb15b8d9dfc9d21e4b140ebc8d87fce8a
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2026-01-12 14:50:41 -0500

    feat: add 2 period panel DDD estimator

--- moderndid/didtriple/__init__.py
+++ moderndid/didtriple/__init__.py
@@ -1,1 +1,13 @@
 """Triple difference-in-differences estimators."""
+
+from moderndid.didtriple.bootstrap.mboot_ddd import mboot_ddd, wboot_ddd
+from moderndid.didtriple.dgp import gen_dgp_2periods
+from moderndid.didtriple.estimators.ddd_panel import DDDPanelResult, ddd_panel
+
+__all__ = [
+    "DDDPanelResult",
+    "ddd_panel",
+    "gen_dgp_2periods",
+    "mboot_ddd",
+    "wboot_ddd",
+]
