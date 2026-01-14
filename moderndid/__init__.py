b0baabdd91d60298df7b0c82fcd6b9a69e1dae4e
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2026-01-14 14:08:26 -0500

    feat: add multi-period DGP functions for staggered treatment adoption

--- moderndid/__init__.py
+++ moderndid/__init__.py
@@ -130,6 +130,8 @@ from moderndid.didtriple import (
     ddd_mp,
     ddd_panel,
     gen_dgp_2periods,
+    gen_dgp_mult_periods,
+    generate_simple_ddd_data,
     mboot_ddd,
     wboot_ddd,
 )
@@ -274,6 +276,8 @@ __all__ = [
     "format_mp_pretest_result",
     "format_mp_result",
     "gen_dgp_2periods",
+    "gen_dgp_mult_periods",
+    "generate_simple_ddd_data",
     "honest_did",
     "ipw_did_panel",
     "ipw_did_rc",
--- moderndid/didtriple/__init__.py
+++ moderndid/didtriple/__init__.py
@@ -3,7 +3,7 @@
 from moderndid.didtriple.agg_ddd import agg_ddd
 from moderndid.didtriple.agg_ddd_obj import DDDAggResult
 from moderndid.didtriple.bootstrap.mboot_ddd import mboot_ddd, wboot_ddd
-from moderndid.didtriple.dgp import gen_dgp_2periods
+from moderndid.didtriple.dgp import gen_dgp_2periods, gen_dgp_mult_periods, generate_simple_ddd_data
 from moderndid.didtriple.estimators.ddd_mp import ATTgtResult, DDDMultiPeriodResult, ddd_mp
 from moderndid.didtriple.estimators.ddd_panel import DDDPanelResult, ddd_panel
 
@@ -16,6 +16,8 @@ __all__ = [
     "ddd_mp",
     "ddd_panel",
     "gen_dgp_2periods",
+    "gen_dgp_mult_periods",
+    "generate_simple_ddd_data",
     "mboot_ddd",
     "wboot_ddd",
 ]
