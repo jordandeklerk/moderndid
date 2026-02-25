8207043ec3ad48273b41b532b1527c294f3c80e2
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2026-02-25 18:36:01 -0500

    REF: remove unused imports and exports from Dask and Spark backends

--- moderndid/dask/__init__.py
+++ moderndid/dask/__init__.py
@@ -1,18 +1,1 @@
 """Dask distributed backend for moderndid estimators."""
-
-from ._utils import get_default_partitions, get_or_create_client, is_dask_collection, validate_dask_input
-from ._ddd import dask_ddd
-from ._did import dask_att_gt
-from ._didcont import dask_cont_did
-from ._didinter import dask_did_multiplegt
-
-__all__ = [
-    "dask_att_gt",
-    "dask_cont_did",
-    "dask_ddd",
-    "dask_did_multiplegt",
-    "get_default_partitions",
-    "get_or_create_client",
-    "is_dask_collection",
-    "validate_dask_input",
-]
